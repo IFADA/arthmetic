13413f0b70b0e04f238c89b1a954d9b4ac61d6a5
boyces <1656959429@qq.com>
2019-09-15 18:12:28 +0800

    Initial commit

---
+++ src/binarysearchtree/Main.java
@@ -0,0 +1,24 @@
+package binarysearchtree;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        BST<Integer> bst = new BST<>();
+        int[] nums = {5, 3, 6, 8, 4};
+        for(int num: nums)
+            bst.add(num);
+
+        /////////////////
+        //      5      //
+        //    /   \    //
+        //   3    6    //
+        //  / \    \   //
+        // 2  4     8  //
+        /////////////////
+        bst.removeMin();
+        bst.postOrder();
+
+       // System.out.println(bst);
+    }
+}
